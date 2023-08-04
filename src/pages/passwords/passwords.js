4098865ad6ae6b7552eb070dfb28c0632b051089
Leonard Marshall Afzal <loma256@gmail.com>
2023-08-04 12:30:38 +0100

    Refactor passwords.js
    - Break source code into smaller functions, seperation of concerns
    - Implement constraints for repeated strings
    - Implement error handling for retrieving credential data

--- src/pages/passwords/passwords.js
+++ src/pages/passwords/passwords.js
@@ -2,62 +2,63 @@ import { db, storage } from "../../../firebase-config.js";
 import { collection, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
 import { getDownloadURL, ref } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-storage.js";
 
-
-// Get user ID from local storage
 const userID = localStorage.getItem('userID');
 
-// If user is logged in, fetch credentials
 const fetchCredentials = async () => {
+  try {
     const querySnapshot = await getDocs(query(collection(db, `accounts`), where('userID', '==', userID)));
-
-    // Get the list element
-    let list = document.getElementById('credentials-list');
-
-    for (const doc of querySnapshot.docs) {
-        // doc.data() is never undefined for query doc snapshots
-        const data = doc.data();
-
-        // Create a list item for each document and add it to the list
-        let listItem = document.createElement('li');
-        listItem.classList.add('list-group-item', 'd-flex', 'align-items-center', 'justify-content-between');
-
-        // Get the download URL for the image
-        let imageRef = ref(storage, data.imageURL); // Assuming data.imageURL is the reference path in storage
-        const url = await getDownloadURL(imageRef);
-
-        // Add the image
-        let image = document.createElement('img');
-        image.src = url;
-        image.alt = data.accountName;
-        image.width = 50; // Set width as needed
-        listItem.appendChild(image);
-
-        // Add the account name
-        let accountName = document.createElement('h5');
-        accountName.textContent = data.accountName;
-        listItem.appendChild(accountName);
-
-        // Add the email/username
-        let email = document.createElement('p');
-        email.textContent = data.email; // Assuming email is the field name in your Firestore document
-        listItem.appendChild(email);
-
-        // Add the button to go to the account URL
-        let button = document.createElement('a');
-        button.href = data.url; 
-        button.classList.add('btn', 'btn-primary');
-
-        let icon = document.createElement('i');
-        icon.classList.add('box', 'box-arrow-right'); 
-        button.appendChild(icon);
-
-        listItem.appendChild(button);
-
-
-
-        list.appendChild(listItem);
-    }
+    const list = document.getElementById('credentials-list');
+    querySnapshot.docs.forEach(doc => renderCredential(list, doc.data()));
+  } catch (error) {
+    console.error("An error occurred while fetching credentials:", error);
+    // Handle the error as needed in your application
+  }
 };
 
-fetchCredentials(); // Call the function to execute the code
+const renderCredential = async (list, data) => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('list-group-item', 'd-flex', 'align-items-center', 'justify-content-between');
+  
+  const imageRef = ref(storage, data.imageURL);
+  const imageUrl = await getDownloadURL(imageRef);
+  listItem.appendChild(createImage(imageUrl, data.accountName));
+  listItem.appendChild(createAccountName(data.accountName));
+  listItem.appendChild(createEmail(data.email));
+  listItem.appendChild(createGoToAccountButton(data.url));
 
+  list.appendChild(listItem);
+};
+
+const createImage = (src, alt) => {
+  const image = document.createElement('img');
+  image.src = src;
+  image.alt = alt;
+  image.width = 50;
+  return image;
+};
+
+const createAccountName = (name) => {
+  const accountName = document.createElement('h5');
+  accountName.textContent = name;
+  return accountName;
+};
+
+const createEmail = (email) => {
+  const emailElement = document.createElement('p');
+  emailElement.textContent = email;
+  return emailElement;
+};
+
+const createGoToAccountButton = (url) => {
+  const button = document.createElement('a');
+  button.href = url;
+  button.classList.add('btn', 'btn-primary');
+
+  const icon = document.createElement('i');
+  icon.classList.add('bi', 'bi-box-arrow-right'); // Adjust class names based on your Bootstrap version
+  button.appendChild(icon);
+
+  return button;
+};
+
+fetchCredentials();
