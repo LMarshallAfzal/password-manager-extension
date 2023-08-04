189a12e7c18532e743d44a97e203c399a8d7b78f
Leonard Marshall Afzal <loma256@gmail.com>
2023-08-04 12:25:49 +0100

    Refactor popup.js
    - Remove redundant semicolon from import statement
    - Add an array with all required file paths

--- src/pages/popup.js
+++ src/pages/popup.js
@@ -1,41 +1,32 @@
 import { auth } from '../../firebase-config.js';
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";;
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+
+const PAGES = [
+  'passwords/passwords.html',
+  'generator/generator.html',
+  'account/account.html'
+];
 
 window.onload = function() {
-    // Load the first page initially
-    document.getElementById('content-placeholder').src = 'src/pages/passwords/passwords.html';
-    
-    // Select the navigation buttons
+    document.getElementById('content-placeholder').src = PAGES[0];
+
     const navButtons = document.querySelectorAll(".btn");
 
-    // Add click event listeners to the navigation buttons
     navButtons.forEach((button, index) => {
         button.addEventListener("click", () => {
-            switch(index) {
-                case 0:
-                    document.getElementById('content-placeholder').src = 'passwords/passwords.html';
-                    break;
-                case 1:
-                    document.getElementById('content-placeholder').src = 'generator/generator.html';
-                    break;
-                case 2:
-                    document.getElementById('content-placeholder').src = 'account/account.html';
-                    break;
-            }
+            document.getElementById('content-placeholder').src = PAGES[index];
         });
     });
 };
 
-// Add an observer on Firebase auth changes
 onAuthStateChanged(auth, (user) => {
     var iframe = document.getElementById('content-placeholder');
   
     if (user) {
-      console.log("User signed in:", user); // Log user object for debugging
-      iframe.src = 'passwords/passwords.html';
+      console.log("User signed in:", user);
+      iframe.src = PAGES[0];
     } else {
-      console.log("No user signed in"); // Log the message for debugging
+      console.log("No user signed in");
       iframe.src = 'login/login.html';
     }
 });
-
