56ef46944008dcdd60b9d5aa8afc793edfc96388
Leonard Marshall Afzal <loma256@gmail.com>
2023-08-04 12:36:26 +0100

    Refactor generator.js
    - Encapsulate settings into an Object
    - Seperate password generation logic into its own function
    - Add user feedback for copying a password

--- src/pages/generator/generator.js
+++ src/pages/generator/generator.js
@@ -1,39 +1,46 @@
-// Function to generate password
-function generate() {
-    const length = document.getElementById("length").value;
-    const includeLetters = document.getElementById("letters").checked;
-    const includeNumbers = document.getElementById("numbers").checked;
-    const includeSymbols = document.getElementById("symbols").checked;
-
-    // Display the current length
-    document.getElementById("length-display").textContent = length;
-
-    let characters = "";
-    if (includeLetters) characters += "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    if (includeNumbers) characters += "0123456789";
-    if (includeSymbols) characters += "!@#$%^&*()_+-=[]{};:',.<>/?`~";
-
-    let password = "";
-    for (let i = 0; i < length; i++) {
-        password += characters[Math.floor(Math.random() * characters.length)];
-    }
-
-    document.getElementById("password").value = password;
+function getSettings() {
+    return {
+        length: document.getElementById("length").value,
+        includeLetters: document.getElementById("letters").checked,
+        includeNumbers: document.getElementById("numbers").checked,
+        includeSymbols: document.getElementById("symbols").checked,
+    };
 }
 
-// Function to copy password to clipboard
+function generatePassword(settings) {
+    let characters = "";
+    if (settings.includeLetters) characters += "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (settings.includeNumbers) characters += "0123456789";
+    if (settings.includeSymbols) characters += "!@#$%^&*()_+-=[]{};:',.<>/?`~";
+
+    let password = "";
+    for (let i = 0; i < settings.length; i++) {
+        password += characters[Math.floor(Math.random() * characters.length)];
+    }
+    return password;
+}
+
+function displayPassword() {
+    const settings = getSettings();
+    const password = generatePassword(settings);
+    document.getElementById("password").value = password;
+    document.getElementById("length-display").textContent = settings.length; // Display current length
+}
+
 function copyPassword() {
     const passwordField = document.getElementById("password");
-    navigator.clipboard.writeText(passwordField.value);
+    navigator.clipboard.writeText(passwordField.value).then(() => {
+        alert("Password copied to clipboard!"); // Provide feedback
+    });
 }
 
 // Generate password when page loads
-window.onload = generate;
+window.onload = displayPassword;
 
 // Add event listeners to inputs to regenerate password when their value changes
-document.getElementById("length").addEventListener("input", generate);
-document.getElementById("letters").addEventListener("change", generate);
-document.getElementById("numbers").addEventListener("change", generate);
-document.getElementById("symbols").addEventListener("change", generate);
+["length", "letters", "numbers", "symbols"].forEach(id => {
+    document.getElementById(id).addEventListener("input", displayPassword);
+    document.getElementById(id).addEventListener("change", displayPassword);
+});
 
 document.getElementById("copy").addEventListener("click", copyPassword);
